ec782a5853f9946ffaffa4385049f537ec324cf8
benicio <beniciobrandao@hotmail.com>
2025-03-29 19:13:34 -0300

    Atualiza .gitignore, remove bin e obj do repositório e adiciona arquivos de projeto

---
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers;
+[Route("api/skills")]
+[ApiController]
+public class SkillsController : ControllerBase
+{
+    // GET api/skills
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+
+        return Ok();
+    }
+
+    // post api/skills
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok();
+    }
+}
