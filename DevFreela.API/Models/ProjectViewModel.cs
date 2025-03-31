150c3d414cabc52494f5a5dc9cff432c2e2333e3
benicio <beniciobrandao@hotmail.com>
2025-03-31 14:35:38 -0300

    Adiciona Model para ProjectController

---
+++ DevFreela.API/Models/ProjectViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.API.Entities;
+
+namespace DevFreela.API.Models;
+
+public class ProjectViewModel
+{
+    public ProjectViewModel(int id, string title, string description, int idClient, int idFreelancer, decimal totalCost, List<ProjectComment> comments)
+    {
+        Id = Id;
+        Title = title;
+        Description = description;
+        IdClient = idClient;
+        IdFreelancer = idFreelancer;
+        TotalCost = totalCost;
+        Comments = comments.Select(c => c.Content).ToList();
+
+    }
+    public int Id {  get; private set; }
+    public string Title {  get; private set; }
+    public string Description { get; private set; }
+    public int IdClient {  get; private set; }
+    public int IdFreelancer {  get; private set; }
+    public string ClientName {  get; private set; }
+    public string FreelancerName {  get; private set; }
+    public decimal TotalCost {  get; private set; }
+    public List<string> Comments {  get; private set; }
+}
