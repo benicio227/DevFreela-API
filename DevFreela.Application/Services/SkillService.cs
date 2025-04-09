e1a8de1942b8729a843255b0c67d6a6bce6fa055
benicio <beniciobrandao@hotmail.com>
2025-04-09 07:16:51 -0300

    Cria o arquivo que implementa a interface ISkillService

---
+++ DevFreela.Application/Services/SkillService.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+
+namespace DevFreela.Application.Services;
+public class SkillService : ISkillService
+{
+    private readonly DevFreelaDbContext _context;
+
+    public SkillService(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+    public ResultViewModel<List<CreateSkillInputModel>> GetAll()
+    {
+        var skills = _context.Skills.ToList();
+
+        var model = skills.Select(CreateSkillInputModel.FromEntity).ToList();
+
+        return ResultViewModel<List<CreateSkillInputModel>>.Success(model);
+    }
+
+    public ResultViewModel<int> Insert(CreateSkillInputModel model)
+    {
+ 
+        var skill = model.ToEntity();
+
+        _context.Skills.Add(skill);
+        _context.SaveChanges();
+
+        return ResultViewModel<int>.Success(skill.Id);
+    }
+}
