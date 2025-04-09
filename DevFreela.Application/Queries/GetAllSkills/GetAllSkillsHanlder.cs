1d2e62e9b6aa1b82921cdb7ee30143c2c2ec3445
benicio <beniciobrandao@hotmail.com>
2025-04-09 10:18:25 -0300

    Cria a pasta GetAllSkills e as classes usadas com MediatR

---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsHanlder.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Queries.GetAllSkills;
+public class GetAllSkillsHandler : IRequestHandler<GetAllSkillsQuery, ResultViewModel<List<CreateSkillInputModel>>>
+{
+    private readonly DevFreelaDbContext _context;
+
+    public GetAllSkillsHandler(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<ResultViewModel<List<CreateSkillInputModel>>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+    {
+        var skills = await _context.Skills.ToListAsync();
+
+        var model = skills.Select(CreateSkillInputModel.FromEntity).ToList();
+
+        return ResultViewModel<List<CreateSkillInputModel>>.Success(model);
+    }
+}
---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQuery.cs
@@ -0,0 +1,7 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Queries.GetAllSkills;
+public class GetAllSkillsQuery : IRequest<ResultViewModel<List<CreateSkillInputModel>>>
+{
+}
