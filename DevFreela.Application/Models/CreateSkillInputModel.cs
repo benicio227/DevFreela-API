b6fd1b83f5029cd0df56b557239befdcc30e744c
benicio <beniciobrandao@hotmail.com>
2025-04-09 07:09:22 -0300

    Cria o método ToEntity para mapeamento

--- DevFreela.Application/Models/CreateSkillInputModel.cs
+++ DevFreela.Application/Models/CreateSkillInputModel.cs
@@ -1,6 +1,20 @@
-﻿namespace DevFreela.Application.Models;
+﻿using DevFreela.Core.Entities;
+using System.Net.Sockets;
+
+namespace DevFreela.Application.Models;
 
 public class CreateSkillInputModel
 {
+
+    public CreateSkillInputModel(string description)
+    {
+        Description = description;
+    }
     public string Description {  get; set; }
+
+    public static CreateSkillInputModel FromEntity(Skill skill)
+        => new(skill.Description);
+
+    public Skill ToEntity()
+    => new(Description);
 }
