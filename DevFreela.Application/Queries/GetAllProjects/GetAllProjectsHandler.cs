6303bed6b901960be59ff8777cc200e6fe182dae
benicio <beniciobrandao@hotmail.com>
2025-04-09 07:01:41 -0300

    Cria a pasta Queries e implementa as classes usando MediatR

---
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsHandler.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Queries.GetAllProjects;
+
+public class GetAllProjectsHandler : IRequestHandler<GetAllProjectsQuery, ResultViewModel<List<ProjectItemViewModel>>>
+{
+    private readonly DevFreelaDbContext _context;
+    public GetAllProjectsHandler(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<ResultViewModel<List<ProjectItemViewModel>>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
+    {
+        var projects = await _context.Projects
+            .Include(p => p.Client)
+            .Include(p => p.Freelancer)
+            .Where(p => !p.IsDeleted)
+            .ToListAsync();
+
+        var model = projects.Select(ProjectItemViewModel.FromEntity).ToList();
+        // O método Select pega cada objeto da lista de projects e passa para o método FromEntity(que está espe
+        // rando no parâmetro um objeto do tipo Project)
+        // E dentro do método é feita a conversão das propriedades da entidade Project para o model
+
+        return ResultViewModel<List<ProjectItemViewModel>>.Success(model);
+    }
+}
+
---
+++ DevFreela.Application/Queries/GetProjectById/GetProjectByIdQuery.cs
@@ -0,0 +1,42 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Queries.GetProjectById;
+public class GetProjectByIdQuery : IRequest<ResultViewModel<ProjectViewModel>>
+{
+    public GetProjectByIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
+
+public class GetProjectByIdHanlder : IRequestHandler<GetProjectByIdQuery, ResultViewModel<ProjectViewModel>>
+{
+    private readonly DevFreelaDbContext _context;
+    public GetProjectByIdHanlder(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<ResultViewModel<ProjectViewModel>> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
+    {
+        var project = await _context.Projects
+            .Include(p => p.Client)
+            .Include(p => p.Freelancer)
+            .Include(p => p.Comments)
+            .ThenInclude(c => c.User)
+            .SingleOrDefaultAsync(p => p.Id == request.Id);
+
+        if (project is null)
+        {
+            return ResultViewModel<ProjectViewModel>.Error("Projeto não existe");
+        }
+
+        var model = ProjectViewModel.FromEntity(project);
+
+        return ResultViewModel<ProjectViewModel>.Success(model);
+    }
+}
