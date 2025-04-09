7394e4c1284246335b67ada27fe84ad5f93e9433
benicio <beniciobrandao@hotmail.com>
2025-04-09 10:07:45 -0300

    Cria a pasta SkillFolder e as classes usando MediatR

---
+++ DevFreela.Application/Commands/SkillFolder/InsertSkill/InsertSkillCommand.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using MediatR;
+
+namespace DevFreela.Application.Commands.SkillFolder.InsertSkill;
+public class InsertSkillCommand : IRequest<ResultViewModel<int>>
+{
+    public InsertSkillCommand(string description)
+    {
+        Description = description;
+    }
+
+    public string Description { get; set; }
+
+    public static CreateSkillInputModel FromEntity(Skill skill)
+    => new(skill.Description);
+
+    public Skill ToEntity()
+    => new(Description);
+}
