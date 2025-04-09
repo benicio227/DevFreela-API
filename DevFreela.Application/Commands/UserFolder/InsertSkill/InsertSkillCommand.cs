9a6c23e18208feac0571c5e174fd037ea26f5b0b
benicio <beniciobrandao@hotmail.com>
2025-04-09 08:50:03 -0300

    Cria a pasta UserFolder e as classes usando o MediatR

---
+++ DevFreela.Application/Commands/UserFolder/InsertSkill/InsertSkillCommand.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Commands.UserFolder.InsertSkill;
+public class InsertSkillCommand : IRequest<ResultViewModel<UserSkillsInputModel>>
+{
+    public int Id { get; set; }
+    public int[] SkillsIds { get; set; }
+}
---
+++ DevFreela.Application/Commands/UserFolder/InsertSkill/InsertSkillHandler.cs
@@ -0,0 +1,42 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Commands.UserFolder.InsertSkill;
+public class InsertSkillHandler : IRequestHandler<InsertSkillCommand, ResultViewModel<UserSkillsInputModel>>
+{
+    private readonly DevFreelaDbContext _context;
+    public InsertSkillHandler(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<ResultViewModel<UserSkillsInputModel>> Handle(InsertSkillCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _context.Skills.SingleOrDefaultAsync(u => u.Id == request.Id);
+
+        if (user is null)
+        {
+            return (ResultViewModel<UserSkillsInputModel>)ResultViewModel.Error("Usuário não encontrado");
+        }
+
+        foreach (var skillId in request.SkillsIds)
+        {
+            var skill = _context.Skills.SingleOrDefault(s => s.Id == skillId);
+
+            if (skill is null)
+            {
+                return (ResultViewModel<UserSkillsInputModel>)ResultViewModel.Error($"Skill com Id {skillId} não encontrada");
+            }
+
+            var userSkill = new UserSkill(request.Id, skillId);
+
+            _context.UserSkills.Add(userSkill);
+        }
+
+        _context.SaveChanges();
+
+        return (ResultViewModel<UserSkillsInputModel>)ResultViewModel.Success();
+    }
+}
