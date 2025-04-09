0e256f311f2f068d5de54b6df09d8a9d2d547268
benicio <beniciobrandao@hotmail.com>
2025-04-09 07:00:21 -0300

    Cria a pasta Commands e implementa as classes usando MediatR

---
+++ DevFreela.Application/Commands/CompleteProject/CompleteProjectCommand.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Commands.CompleteProject;
+public class CompleteProjectCommand : IRequest<ResultViewModel>
+ // O IRequest é uma interface do MediatR e nós vamos passar um tipo para ele que vai ser o tipo de retorno.
+ // Isso vai ser utilizado para que o próprio MediatR saiba para qual Handler ele vai delegar esse Command para ser
+ // executado
+{
+    public CompleteProjectCommand(int id)
+    {
+        Id = id;
+    }
+    public int Id { get; set; }
+}
