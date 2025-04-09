0559ed622a1022b3c22a57655ecf38d59017fa23
benicio <beniciobrandao@hotmail.com>
2025-04-09 07:27:31 -0300

    Altera nomenclatura do arquivo

--- DevFreela.Application/ApplicationModule.cs
+++ DevFreela.Application/ApplicationModule.cs
@@ -1,4 +1,4 @@
-﻿using DevFreela.Application.Commands.InsertProject;
+﻿using DevFreela.Application.Commands.Project.InsertProject;
 using DevFreela.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
