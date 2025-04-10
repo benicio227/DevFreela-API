b0301e02f01403df2d21704dfcb0ef7b34c1377e
benicio <beniciobrandao@hotmail.com>
2025-04-10 13:49:21 -0300

    Cria a interface para IProjectRepository

---
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Core.Entities;
+using System.Runtime.InteropServices;
+
+namespace DevFreela.Core.Repositories;
+public interface IProjectRepository
+{
+    Task<List<Project>> GetAll();
+    Task<Project?> GetDeatilsById(int id);
+    Task<Project?> GetById(int id);
+    Task<int> Add(Project project);
+    Task Update(Project project);
+    Task AddComment(ProjectComment comment);
+    Task<bool> Exists(int id);
+}
