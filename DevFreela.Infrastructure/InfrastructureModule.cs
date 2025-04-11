abfc67bbdf1ed55485c13d410b131062456819df
benicio <beniciobrandao@hotmail.com>
2025-04-11 11:26:30 -0300

    Cria a classe InfrastructureModule para registrar o serviço Repository como injeção de dependência

---
+++ DevFreela.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevFreela.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddRepositories();
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+
+            return services;
+        }
+    }
+}
