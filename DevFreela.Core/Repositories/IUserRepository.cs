2041758c44c60d34ad52decab00233a9c1174900
benicio <beniciobrandao@hotmail.com>
2025-04-11 15:28:49 -0300

    Cria a interface para o Repositorio

---
+++ DevFreela.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task<User?> GetById(int id);
+        public Task<int> Add(User user);
+        public Task<Skill?> AddPostSkill(int id);
+        public Task<UserSkill> AddUserSkill(UserSkill user);
+    }
+}
