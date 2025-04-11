161bda26a9a606f369fbc44d2a5049839e04a000
benicio <beniciobrandao@hotmail.com>
2025-04-11 15:45:26 -0300

    Implementa o acesso a dados para entidade Skill

---
+++ DevFreela.Infrastructure/Repositories/SkillRepository.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+
+namespace DevFreela.Infrastructure.Repositories;
+public class SkillRepository : ISkillRepository
+{
+    private readonly DevFreelaDbContext _context;
+
+    public SkillRepository(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<int> Add(Skill skill)
+    {
+        var skillExist = await _context.Skills.AddAsync(skill);
+
+        return skill.Id;
+    }
+
+    public Task<List<Skill>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Skill?> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
