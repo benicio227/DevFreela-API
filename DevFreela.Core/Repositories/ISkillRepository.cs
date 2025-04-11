a67f1f52815fdb42a8708afea6441ab0d2075e72
benicio <beniciobrandao@hotmail.com>
2025-04-11 15:44:36 -0300

    Cria a interface para utilizar o Repository

---
+++ DevFreela.Core/Repositories/ISkillRepository.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Core.Repositories;
+public interface ISkillRepository
+{
+    public Task<int> Add(Skill skill);
+    public Task<List<Skill>> GetAll();
+    public Task<Skill?> GetById(int id);
+}
