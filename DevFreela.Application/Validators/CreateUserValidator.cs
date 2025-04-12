f415b9fdc26b03b7890f591edba7e778947c42c3
benicio <beniciobrandao@hotmail.com>
2025-04-11 22:07:54 -0300

    Cria pasta para validação de dados usando FluentValidator

---
+++ DevFreela.Application/Validators/CreateUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Application.Commands.UserFolder.InsertUser;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators;
+public class CreateUserValidator : AbstractValidator<InsertUserCommand>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(u => u.Email)
+            .EmailAddress()
+                .WithMessage("E-mail inválido");
+
+        RuleFor(u => u.BirthDate)
+            .Must(d => d < DateTime.Now.AddYears(-18))
+                .WithMessage("Deve ser maior de idade");
+    }
+}
